9c6c7eb2abf83ab75d1fdc2bb97014190a9f004f
Pietroarmill <pietroarmillotta38@gmail.com>
2022-04-07 15:49:35 +0200

    prezzo a schermo

--- js/script.js
+++ js/script.js
@@ -3,9 +3,6 @@
 // sconto per minorenni: 20%
 // sconto per over 65: 40%
 const priceKm = 0.21;
-
-// const discountOver = 40%
-// const discountUnder = 20%
 console.log(priceKm);
 
 //chiedere all'utente la sua eta e i chilometri che vuole percorrere 
@@ -18,5 +15,21 @@ let ticketPrice = tripLength * priceKm;
 console.log(ticketPrice);
 
 //applicare gli sconti 
+let tripPrice;
 
-//
+if (yourAge < 18 ) {
+    tripPrice = (ticketPrice - ((ticketPrice * 20) / 100));
+}   else if (yourAge > 65) {
+    tripPrice = (ticketPrice - ((ticketPrice * 40) / 100));
+}   else {
+    tripPrice = ticketPrice ;
+}
+
+console.log(tripPrice);
+
+const tripPriceFormatted = tripPrice.toFixed(2) + " €";
+console.log(tripPriceFormatted);
+
+
+//far vedere a schermo il prezzo complessivo del biglietto
+document.getElementById('price').innerHTML = `Costo del biglietto: ${tripPriceFormatted}`;
